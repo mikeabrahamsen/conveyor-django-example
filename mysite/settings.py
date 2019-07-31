c590201ea58e08d178f8bb84268b7aaee415b9aa
Michael Abrahamsen <mail@michaelabrahamsen.com>
2019-07-30 19:04:03 -0600

    use env_var for secret_key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g5en+6l++hukc824ff6)%$q^_f@p0f4-ft2tsuctlj%6yw3$q6'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
