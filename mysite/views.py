02619719e9514b5927c30af275eef148b4c7d20f
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-03-15 22:26:52 -0600

    wip

--- mysite/views.py
+++ mysite/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse(
-        f"Congratulations you launched a Django application {socket.gethostname()}"
+        f"Congratulations you launched a Django application {socket.gethostname()}."
     )
